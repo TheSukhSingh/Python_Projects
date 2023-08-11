1f589f4caeada98e09fc4682bf0a272d4d799564
TheSukhSingh <130133318+TheSukhSingh@users.noreply.github.com>
2023-08-11 12:17:31 +0530

    .

---
+++ 002. Word Guessing Game.py
@@ -0,0 +1,37 @@
+import random
+
+words = ['rainbow', 'computer', 'science', 'programming','python', 'mathematics', 'player', 'condition','reverse', 'water', 'board', 'geeks']
+
+word = random.choice(words)
+
+
+print("Guess the characters")
+
+guesses = ''
+
+turns = 12
+
+while turns > 0:
+  failed = 0
+    
+  for char in word:
+    if char in guesses:
+      print(char)
+                
+    else:
+      print("_")
+      failed += 1
+
+  if failed == 0:
+    print("\nYou won!\nThe word is",word)
+    break
+    
+  guess = input("Enter your guess: ")
+  guesses += guess
+    
+  if guess not in word:
+    turns -= 1
+    if turns == 0:
+      print("You lose!")
+        
+    
