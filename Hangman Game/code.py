109f649121c667eb055cca9571a24e332d470cb1
TheSukhSingh <130133318+TheSukhSingh@users.noreply.github.com>
2023-09-15 00:27:17 +0530

    .

--- Hangman Game/code.py
+++ Hangman Game/code.py
@@ -1,21 +1,37 @@
 import random
 
-def printword(word, guess):
-    
-    print()
+fruits = '''muskmelon watermelon apple banana guava lemon strawberry papaya mango orange grape peach berry'''
+fruits = fruits.split(' ')
 
-someWords = '''apple banana mango strawberry orange grape pineapple apricot lemon coconut watermelon cherry papaya berry peach lychee muskmelon'''
-someWords = someWords.split(' ')
+fruit = random.choice(fruits)
 
-word = random.choice(someWords)
+length = len(fruit)
+tries = length + 2
+print(f'Welcome to Hangman Game.\nThe word is {length} letters long, and you get {tries} tries.')
 
-print('This is a Game. Guess the word and win prizes')
-leng = len(word)
-tries = leng + 2
-print(f'This is {leng} letter word! So you get {tries} tries!')
-guess = ''
-
+guessed = ''
 while tries:
-    tries -= 1
-    printword(word, guess)
-
+    chk = 0
+    for word in fruit:
+        if word in guessed:
+            print(f'{word}', end = " ")
+        else:
+            print('-', end = " ")
+            chk += 1
+    print(f'\n{chk}')
+    if chk == 0:
+        break
+    else:
+        pass
+    guess = input('\nEnter your guess: ')
+    guessed += guess
+    if guess in fruit:
+        print('Correct Guess')
+    else:
+        tries -= 1
+        print(f'Wrong guess\nYou have {tries} tries')
+    
+if chk > 0:
+    print(f'You lost! The word was: {fruit}')
+else:
+    print(f'You won! The word was {fruit}')
